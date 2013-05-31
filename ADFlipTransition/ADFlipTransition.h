c1bfc29562033c4890971a48feaafa199a508d01
Adam Debono <adamdebono@gmail.com>
2013-05-31 18:07:22 +1000

    Initial Commit

---
+++ ADFlipTransition/ADFlipTransition.h
@@ -0,0 +1,13 @@
+//
+//  ADFlipTransition.h
+//  ADFlipTransition
+//
+//  Created by Adam Debono on 31/05/13.
+//  Copyright (c) 2013 Adam Debono. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADFlipTransition : NSObject
+
+@end
